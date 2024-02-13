95a9c364a0641fa89755f0601b6d3753b8a221da
Evan Donkus <evdo5141@colorado.edu>
2024-02-13 01:28:56 +0000

    Added test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+wget -O SkinnyTasteR.html https://www.skinnytaste.com/white-chicken-chili/
+grepout=$(grep -ia "print" SkinnyTasteR.html | grep -Eo 'href=\"https?:\/\/[^"]+\"'| grep -Eo '\"https?:\/\/[^"]+\"')
+temp="${grepout%\"}"
+echo $temp
+recipeurl="${temp#\"}"
+echo $recipeurl
+wget -O SkinnyRecipeFinal.html $recipeurl
