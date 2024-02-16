496a3a464d762fc4fd6979cbe2328d578a147e15
Evan Donkus <evdo5141@colorado.edu>
2024-02-16 21:54:51 +0000

    Adding my comments and notes to self

--- extract_scripts/pythonTest.py
+++ extract_scripts/pythonTest.py
@@ -1,5 +1,8 @@
 from html.parser import HTMLParser
 
+##-----------------------
+## HTML Parser reads in  an html file and parses between data and tags
+#-----------------------
 class MyHTMLParser(HTMLParser):
     def  __init__(self):
         HTMLParser.__init__(self)
@@ -15,8 +18,27 @@ class MyHTMLParser(HTMLParser):
         print("Data :", data)
         self.dataList.append(data)
         
-    
+#-------------    
+# Run shell script to get Instruction list into own html file
+#-------------
 
+
+#-------------
+# Instantiate parser and run on instruction list
+#-------------
+
+
+
+#-------------
+# Take dataList and write to file
+#-------------
+
+
+
+# Notes:
+# Need to make sure python knows where files are being created
+#  Might be a case for further separating bash script into separate bash scripts
+#  
 with open("temp.html", "r") as htmlfile:
     test=htmlfile.read()
         
