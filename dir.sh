9634ebe991e269252dc9f868dd68207ced1bdbbe
Hallee Ray <hara7620@colorado.edu>
2024-02-18 21:38:57 +0000

    Updated script notes

--- dir.sh
+++ dir.sh
@@ -1,7 +1,30 @@
-# build directory heirarchy for recipe
-# call dir.sh with name of recipe to build hierarchy
+#!/bin/bash
+# Script:
+#  dir.sh
+#
+# Purpose:
+#   Build a directory hierarchy to store files for each recipe
+#
+# Inputs:
+#   $1 = Recipe name
+#
+# Outputs:
+#   Print what directory has been created
+#
+# Notes:
+#   Assumes working directory is the home directory of the repository
+#
+# Exits
+#   0 = success
+#   1 = no name entered for directory creation
+#    
+# Changelog:
+#    Date (MM-DD-YYYY)     Name      Change Description
+#    02-18-2024            halleeray   Initial Creation
+#
+#------------------------------------------------------------------
+#------------------------------------------------------------------
 
-#!bin/bash
 
 if [ -z "$1" ]; then
 	echo "Please enter the recipe name to build a directory."
