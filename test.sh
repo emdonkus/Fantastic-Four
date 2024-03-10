ab91d1c838f530ec1a72ce0d632531f3bb569d5a
Evan Donkus <evdo5141@colorado.edu>
2024-03-10 14:02:46 +0000

    Removing unneeded files

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-filepath='Recipes/budgetbytes/Apple_Cranberry_Baked_Oatmeal/Apple_Cranberry_Baked_Oatmeal_ingredients.txt'
-ls $filepath
-if [[ -s $filepath ]];
-then
-    echo "GOOD"
-else
-    echo "BAD"
-fi
