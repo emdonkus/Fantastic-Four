1369f79f3b3f94538236aa4b14f88fd550a4e2a7
Hallee Ray <hara7620@colorado.edu>
2024-04-28 14:55:19 +0000

    removing prefix for render

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Insert the wrapper for handling PROXY when using csel.io virtual machine
 # Calling this routine will have no effect if running on local machine
-prefix.use_PrefixMiddleware(app)   
+# prefix.use_PrefixMiddleware(app)   
 
 # test route to show prefix settings
 # @app.route('/prefix_url')  
